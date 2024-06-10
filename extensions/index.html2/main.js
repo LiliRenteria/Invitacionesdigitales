6e5bef6b33c6fa69054a5a91b845d7a2ae0c8a9e
LiliRentería <lilyrenteria7937@gmail.com>
2024-06-10 12:56:27 -0600

    este es mi primer proyecto

---
+++ extensions/index.html2/main.js
@@ -0,0 +1,16 @@
+function valoraredad() {
+    let inputEdad = document.getElementById("txtEdad")
+    let edad = inputEdad.value
+}
+if(edad)
+
+if(edad>10) {
+console.log(es positivo)
+}
+else{
+    console.log(tu numero es negativo)
+} else{
+    console.log("invalido")
+
+}
+//alert(edad)
